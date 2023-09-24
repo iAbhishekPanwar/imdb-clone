a27922c4b9c95879ab815cded5e4242d92a2a000
Abhishek Panwar <imabhishek612@gmail.com>
2023-09-24 15:16:26 +0530

    navbar done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import NavbarItem from "./NavbarItem";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <div className="flex justify-center dark:bg-gray-600 bg-amber-100 lg:text-lg p-4">
+      <NavbarItem title="Trending" param="fetchTrending" />
+      <NavbarItem title="Top Rated" param="fetchTopRated" />
+    </div>
+  );
 };
 
 export default NavBar;
